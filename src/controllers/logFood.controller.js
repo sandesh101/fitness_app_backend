b62cd58b41ea229354d651e75930959b5bf88ce6
Sandesh Rimal <sandesh.rimal@eydean.com>
2024-02-17 11:27:20 +0545

    Log single food

--- src/controllers/logFood.controller.js
+++ src/controllers/logFood.controller.js
@@ -1,119 +1,52 @@
-// import { LoggedFood } from "../models/logFood.model.js";
-// import { ApiResponse } from "../utils/ApiResponse.js";
-
-// const logFoodController = async (req, res) => {
-//   try {
-//     const {
-//       name,
-//       calorieValue,
-//       quantity,
-//       carbs,
-//       protein,
-//       fat,
-//       fibre,
-//       iron,
-//       calcium,
-//       vitaminC,
-//       mealTime,
-//     } = req.body;
-//     // if (!name || !calorieValue || !quantity || !mealTime || !carbs || !protein || !fat || !fibre || !iron || !calcium || !vitaminC) {
-//     //   return res.status(400).json({
-//     //     message: "All fields are required",
-//     //   });
-//     // }
-
-//     // Creating the logged food
-//     // const loggedFood = await LoggedFood.create({
-//     //   userId: req.user.id,
-//     //   name,
-//     //   calorieValue,
-//     //   quantity,
-//     //   carbs,
-//     //   protein,
-//     //   fat,
-//     //   fibre,
-//     //   iron,
-//     //   calcium,
-//     //   vitaminC,
-//     //   mealTime,
-//     // });
-
-//     console.log(req.body);
-//   //   if (!loggedFood) {
-//   //     return res.status(400).json(new ApiResponse(400, "Some Error Occurred"));
-//   //   } else {
-//   //     return res.status(201).json(new ApiResponse(200, loggedFood, "Logged Successfully"));
-//   //   }
-//   } 
-//   catch (e) {
-//     console.log(e);
-//     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
-//   }
-// };
-
-// const getLoggedFood = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-    
-//     const loggedFoodDetails = await LoggedFood.find({ userId });
-
-//     if (!loggedFoodDetails) {
-//       return res.status(400).json(new ApiResponse(400, "Error Getting Data"));
-//     } else {
-//       return res.status(200).json(new ApiResponse(200, loggedFoodDetails, "Success"));
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
-//   }
-// };
-
-
-// export { logFoodController, getLoggedFood };
-
-// controllers/logFoodController.js
 import { LoggedFood } from "../models/logFood.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const logFoodController = async (req, res) => {
   try {
-    const { date, meals } = req.body;
-
-    if (!date || !meals || !Array.isArray(meals) || meals.length === 0) {
+    const {
+      name,
+      calorieValue,
+      quantity,
+      carbs,
+      protein,
+      fat,
+      fibre,
+      iron,
+      calcium,
+      vitaminC,
+      mealTime,
+    } = req.body;
+    if (!name || !calorieValue || !quantity || !mealTime || !carbs || !protein || !fat || !fibre || !iron || !calcium || !vitaminC) {
       return res.status(400).json({
-        message: "Invalid request body. 'date' and non-empty array 'meals' are required."
+        message: "All fields are required",
       });
     }
 
-    const userId = req.user.id;
-
-    let foodLog = await LoggedFood.findOne({ userId });
-
-    if (!foodLog) {
-      // If no food log exists for the user, create a new one
-      foodLog = new LoggedFood({ userId, logs: [] });
-    }
-
-    // Check if a log entry already exists for the given date
-    let logEntry = foodLog.logs.find(log => log.date.toISOString().split('T')[0] === date);
-
-    if (!logEntry) {
-      // If no log entry exists for the given date, create a new one
-      logEntry = { date: new Date(date), meals: [] };
-      foodLog.logs.push(logEntry);
-    }
-
-    // Add meals to the log entry
-    meals.forEach(meal => {
-      const { mealTime, foods } = meal;
-      logEntry.meals.push({ mealTime, foods });
+    // Creating the logged food
+    const loggedFood = await LoggedFood.create({
+      userId: req.user.id,
+      name,
+      calorieValue,
+      quantity,
+      carbs,
+      protein,
+      fat,
+      fibre,
+      iron,
+      calcium,
+      vitaminC,
+      mealTime,
     });
 
-    await foodLog.save();
-
-    return res.status(201).json(new ApiResponse(201, foodLog, "Logged successfully"));
-  } catch (error) {
-    console.error(error);
+    // console.log(req.body);
+    if (!loggedFood) {
+      return res.status(400).json(new ApiResponse(400, "Some Error Occurred"));
+    } else {
+      return res.status(201).json(new ApiResponse(200, loggedFood, "Logged Successfully"));
+    }
+  } 
+  catch (e) {
+    console.log(e);
     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
   }
 };
@@ -121,19 +54,86 @@ const logFoodController = async (req, res) => {
 const getLoggedFood = async (req, res) => {
   try {
     const userId = req.user.id;
-    const foodLog = await LoggedFood.findOne({ userId });
+    
+    const loggedFoodDetails = await LoggedFood.find({ userId });
 
-    if (!foodLog) {
-      return res.status(404).json(new ApiResponse(404, "Food log not found for the user"));
+    if (!loggedFoodDetails) {
+      return res.status(400).json(new ApiResponse(400, "Error Getting Data"));
+    } else {
+      return res.status(200).json(new ApiResponse(200, loggedFoodDetails, "Success"));
     }
-
-    return res.status(200).json(new ApiResponse(200, foodLog, "Success"));
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
   }
 };
 
+
 export { logFoodController, getLoggedFood };
+
+// controllers/logFoodController.js
+// import { LoggedFood } from "../models/logFood.model.js";
+// import { ApiResponse } from "../utils/ApiResponse.js";
+
+// const logFoodController = async (req, res) => {
+//   try {
+//     const { date, meals } = req.body;
+
+//     if (!date || !meals || !Array.isArray(meals) || meals.length === 0) {
+//       return res.status(400).json({
+//         message: "Invalid request body. 'date' and non-empty array 'meals' are required."
+//       });
+//     }
+
+//     const userId = req.user.id;
+
+//     let foodLog = await LoggedFood.findOne({ userId });
+
+//     if (!foodLog) {
+//       // If no food log exists for the user, create a new one
+//       foodLog = new LoggedFood({ userId, logs: [] });
+//     }
+
+//     // Check if a log entry already exists for the given date
+//     let logEntry = foodLog.logs.find(log => log.date.toISOString().split('T')[0] === date);
+
+//     if (!logEntry) {
+//       // If no log entry exists for the given date, create a new one
+//       logEntry = { date: new Date(date), meals: [] };
+//       foodLog.logs.push(logEntry);
+//     }
+
+//     // Add meals to the log entry
+//     meals.forEach(meal => {
+//       const { mealTime, foods } = meal;
+//       logEntry.meals.push({ mealTime, foods });
+//     });
+
+//     await foodLog.save();
+
+//     return res.status(201).json(new ApiResponse(201, foodLog, "Logged successfully"));
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
+//   }
+// };
+
+// const getLoggedFood = async (req, res) => {
+//   try {
+//     const userId = req.user.id;
+//     const foodLog = await LoggedFood.findOne({ userId });
+
+//     if (!foodLog) {
+//       return res.status(404).json(new ApiResponse(404, "Food log not found for the user"));
+//     }
+
+//     return res.status(200).json(new ApiResponse(200, foodLog, "Success"));
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
+//   }
+// };
+
+// export { logFoodController, getLoggedFood };
 
 
--- src/models/logFood.model.js
+++ src/models/logFood.model.js
@@ -1,142 +1,138 @@
-// import mongoose from "mongoose";
-// const { Schema } = mongoose;
-
-// const loggedFoodSchema = new Schema(
-//   {
-//     foods: [ 
-//       {
-//       name: {
-//       type: String,
-//       required: true,
-//     },
-//     calorieValue: {
-//       type: Number,
-//       required: true,
-//     },
-//     quantity: {
-//       type: Number,
-//       required: true,
-//     },
-//     // Nutrients
-//     carbs: {
-//       type: Number,
-//       required: true,
-//     },
-//     protein: {
-//       type: Number,
-//       required: true,
-//     },
-//     fat: {
-//       type: Number,
-//       required: true,
-//     },
-//     fibre: {
-//       type: Number,
-//       required: true,
-//     },
-//     iron: {
-//       type: Number,
-//       required: true,
-//     },
-//     calcium: {
-//       type: Number,
-//       required: true,
-//     },
-//     vitaminC: {
-//       type: Number,
-//       required: true,
-//     },
-//     mealTime: {
-//       type: String,
-//       required: true,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   },
-//    ]
-//   }
-// );
-
-// export const LoggedFood = mongoose.model("LoggedFood", loggedFoodSchema);
-
-
-// models/logFood.model.js
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const loggedFoodSchema = new Schema(
   {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true
+      name: {
+      type: String,
+      required: true,
     },
-    logs: [
-      {
-        date: {
-          type: Date,
-          required: true
-        },
-        meals: [
-          {
-            mealTime: {
-              type: String,
-              required: true
-            },
-            foods: [
-              {
-                name: {
-                  type: String,
-                  required: true
-                },
-                calorieValue: {
-                  type: Number,
-                  required: true
-                },
-                quantity: {
-                  type: Number,
-                  required: true
-                },
-                // Nutrients
-                carbs: {
-                  type: Number,
-                  required: true
-                },
-                protein: {
-                  type: Number,
-                  required: true
-                },
-                fat: {
-                  type: Number,
-                  required: true
-                },
-                fibre: {
-                  type: Number,
-                  required: true
-                },
-                iron: {
-                  type: Number,
-                  required: true
-                },
-                calcium: {
-                  type: Number,
-                  required: true
-                },
-                vitaminC: {
-                  type: Number,
-                  required: true
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ]
+    calorieValue: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    // Nutrients
+    carbs: {
+      type: Number,
+      required: true,
+    },
+    protein: {
+      type: Number,
+      required: true,
+    },
+    fat: {
+      type: Number,
+      required: true,
+    },
+    fibre: {
+      type: Number,
+      required: true,
+    },
+    iron: {
+      type: Number,
+      required: true,
+    },
+    calcium: {
+      type: Number,
+      required: true,
+    },
+    vitaminC: {
+      type: Number,
+      required: true,
+    },
+    mealTime: {
+      type: String,
+      required: true,
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
 export const LoggedFood = mongoose.model("LoggedFood", loggedFoodSchema);
+
+
+// models/logFood.model.js
+// import mongoose from "mongoose";
+// const { Schema } = mongoose;
+
+// const loggedFoodSchema = new Schema(
+//   {
+//     userId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true
+//     },
+//     logs: [
+//       {
+//         date: {
+//           type: Date,
+//           required: true
+//         },
+//         meals: [
+//           {
+//             mealTime: {
+//               type: String,
+//               required: true
+//             },
+//             foods: [
+//               {
+//                 name: {
+//                   type: String,
+//                   required: true
+//                 },
+//                 calorieValue: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 quantity: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 // Nutrients
+//                 carbs: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 protein: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 fat: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 fibre: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 iron: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 calcium: {
+//                   type: Number,
+//                   required: true
+//                 },
+//                 vitaminC: {
+//                   type: Number,
+//                   required: true
+//                 }
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   {
+//     timestamps: true
+//   }
+// );
+
+// export const LoggedFood = mongoose.model("LoggedFood", loggedFoodSchema);
