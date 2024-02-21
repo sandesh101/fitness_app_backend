1a03327ba28248295a8eeade205ad4587b977354
Sandesh-softech <sandeshrimal@softechfoundation.com>
2024-02-21 22:23:17 +0545

    Multiple food logs

--- src/controllers/logFood.controller.js
+++ src/controllers/logFood.controller.js
@@ -3,69 +3,37 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const logFoodController = async (req, res) => {
   try {
-    const {
-      name,
-      calorieValue,
-      quantity,
-      carbs,
-      protein,
-      fat,
-      fibre,
-      iron,
-      calcium,
-      vitaminC,
-      mealTime,
-    } = req.body;
-    if (!name || !calorieValue || !quantity || !mealTime || !carbs || !protein || !fat || !fibre || !iron || !calcium || !vitaminC) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
+    const { mealTime, ...foodData } = req.body;
+
+    // Check if mealTime is provided and is one of the allowed values
+    if (!mealTime || !['Breakfast', 'Snacks', 'Lunch', 'Dinner'].includes(mealTime)) {
+      return res.status(400).json({ error: 'Invalid or missing meal time' });
     }
 
+    // Check if food data fields are present and have valid values
+    const requiredFields = ['name', 'calorieValue', 'quantity', 'carbs', 'protein', 'fat', 'fibre', 'iron', 'calcium', 'vitaminC'];
+    for (const field of requiredFields) {
+      if (!(field in foodData) || !foodData[field]) {
+        return res.status(400).json({ error: `Missing or invalid value for field: ${field}` });
+      }
+    }
 
+    const loggedFood = await LoggedFood.create({ mealTime, foodData });
 
-    const currentDate = new Date();
-    const options = {
-      hour12: true,
-      timeZone: 'Asia/Kathmandu', // Nepal Standard Time (NST)
-    };
-
-    const loggedTime = currentDate.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', ...options });
-    const loggedDate = currentDate.toISOString().slice(0, 10); // Extract YYYY-MM-DD
-
-
-    // Creating the logged food
-    const loggedFood = await LoggedFood.create({
-      userId: req.user.id,
-      name,
-      calorieValue,
-      quantity,
-      carbs,
-      protein,
-      fat,
-      fibre,
-      iron,
-      calcium,
-      vitaminC,
-      mealTime,
-    });
-
-    // console.log(req.body);
     if (!loggedFood) {
       return res.status(400).json(new ApiResponse(400, "Some Error Occurred"));
     } else {
       return res.status(201).json(new ApiResponse(200, loggedFood, "Logged Successfully"));
     }
-  }
-  catch (e) {
-    console.log(e);
-    return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
   }
 };
 
 const getLoggedFood = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
 
     const loggedFoodDetails = await LoggedFood.find({ userId });
 
@@ -79,6 +47,5 @@ const getLoggedFood = async (req, res) => {
     return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
   }
 };
-
 
 export { logFoodController, getLoggedFood };
--- src/models/logFood.model.js
+++ src/models/logFood.model.js
@@ -3,51 +3,59 @@ const { Schema } = mongoose;
 
 const loggedFoodSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    calorieValue: {
-      type: Number,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    // Nutrients
-    carbs: {
-      type: Number,
-      required: true,
-    },
-    protein: {
-      type: Number,
-      required: true,
-    },
-    fat: {
-      type: Number,
-      required: true,
-    },
-    fibre: {
-      type: Number,
-      required: true,
-    },
-    iron: {
-      type: Number,
-      required: true,
-    },
-    calcium: {
-      type: Number,
-      required: true,
-    },
-    vitaminC: {
-      type: Number,
-      required: true,
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     mealTime: {
       type: String,
       required: true,
+      enum: ['Breakfast', 'Snacks', 'Lunch', 'Dinner']
     },
+    foodData: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        calorieValue: {
+          type: Number,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        carbs: {
+          type: Number,
+          required: true,
+        },
+        protein: {
+          type: Number,
+          required: true,
+        },
+        fat: {
+          type: Number,
+          required: true,
+        },
+        fibre: {
+          type: Number,
+          required: true,
+        },
+        iron: {
+          type: Number,
+          required: true,
+        },
+        calcium: {
+          type: Number,
+          required: true,
+        },
+        vitaminC: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
